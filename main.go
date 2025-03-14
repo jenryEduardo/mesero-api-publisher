671b95d7f81aaf5e1600482dcc5e663a46cb4e30
diedgo05 <233358@ids.upchiapas.edu.mx>
2025-03-14 12:52:38 -0600

    Segundo intento actions

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
-
 package main
 
 import (
 	"log"
+	"net/http"
 
 	countsRoutes "publisher/src/infraestructure/routes"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -22,9 +23,13 @@ func main(){
 	}))
 
 	countsRoutes.SetUp(router)
-	
 
+	router.GET("/status", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, map[string]string{"status": "OK"})
+	})
+	
 	port := ":8080"
+	router.Run(port)
 	log.Println("Servidor escuchando en el puerto", port)
 	log.Fatal(router.Run(port))
 
