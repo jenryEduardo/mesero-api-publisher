71341c3688cd24da035ba487cb10fb404629ad0f
Adrián10-07 <adrian2aguilarg@gmail.com>
2025-03-15 13:57:30 -0600

    aporte

--- src/domain/Ipedido.go
+++ src/domain/Ipedido.go
@@ -2,7 +2,4 @@ package domain
 
 type Ipedido interface{
 	RealizarPedido(pedido *Pedido)error
-	// VerPedido()([]Pedido,error)
-	// BorrarPedido(id int)error
-	// ActualizarPedido(id int,pedido *Pedido)error
 }
